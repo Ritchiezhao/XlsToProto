792d62e088088d0b86bb123c24ea1c19b2008bde
Ritchiezhao <592708927@qq.com>
2017-10-27 11:21:00 +0800

    delete

--- general_person.bat
+++
@@ -1,4 +0,0 @@
-call python xls_deploy_tool.py PERSON xls/person.xls
-call protoc proto/tnt_deploy_person.proto --descriptor_set_out=desc/person.protodesc
-call ProtoGen\protogen -i:desc/person.protodesc -o:cs/person.cs
-pause
